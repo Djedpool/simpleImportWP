916e362849b883c7b49ceff66e057fb644015205
Stefan Oljacic <djedpool@gmail.com>
2021-11-17 23:36:24 +0100

    working on stuff.

--- displaylist.php
+++ displaylist.php
@@ -16,46 +16,51 @@ if(isset($_POST['butimport'])){
 
     $csvFile = fopen($_FILES['import_file']['tmp_name'], 'r');
 
-    $file = fgetcsv($csvFile);
+    fgetcsv($csvFile);
 
-    echo '<pre>' . var_export($csvFile, true) . '</pre>';
-    exit();
+    $arr = [];
 
-    while(($csvData = fgetcsv($csvFile)) !== FALSE){
+    while(($csvData =fgetcsv($csvFile)) !== FALSE){
 
+        $csvData = array_map("utf8_encode", $csvData);
 
+        $dataLen = count($csvData);
 
-      $csvData = array_map("utf8_encode", $csvData);
+        $data = [];
 
-
-
-      $dataLen = count($csvData);
-
-      $workoutType = trim($csvData[1]);
-      $distance = trim($csvData[22]);
-      $exerciseTime = trim($csvData[34]);
-
-      $cntSQL = "SELECT count(*) as count FROM {$tablename} where username='".$username."'";
-      $record = $wpdb->get_results($cntSQL, OBJECT);
-
-      if($record[0]->count==0){
-
-        if(!empty($name) && !empty($username) && !empty($email) && !empty($age) ) {
-
-          $wpdb->insert($tablename, array(
-            'name' =>$fullName,
-            'email' =>$email,
-            'age' => $age
-          ));
-
-          if($wpdb->insert_id > 0){
-            $totalInserted++;
-          }
+        if(trim($csvData[1]) === 'Run' ||  trim($csvData[1]) === 'Swim' || trim($csvData[1]) === 'Bike') {
+            $data['workout_type'] = trim($csvData[1]);
+            $data['distance'] = trim($csvData[7]);
+            $data['exerciseTime'] = trim($csvData[12]);
+            
+            $arr[$totalInserted] = $data;
         }
 
-      }
-
+        $totalInserted++;
     }
+
+    echo '<pre>' . var_export($arr, true) . '</pre>';
+    exit();
+
+    $cntSQL = "SELECT count(*) as count FROM {$tablename} where username='".$username."'";
+    $record = $wpdb->get_results($cntSQL, OBJECT);
+
+    if($record[0]->count==0){
+
+      if(!empty($name) && !empty($username) && !empty($email) && !empty($age) ) {
+
+        $wpdb->insert($tablename, array(
+          'name' =>$fullName,
+          'email' =>$email,
+          'age' => $age
+        ));
+
+        if($wpdb->insert_id > 0){
+
+        }
+      }
+    }
+
     echo "<h3 style='color: green;'>Total record Inserted : ".$totalInserted."</h3>";
 
 
